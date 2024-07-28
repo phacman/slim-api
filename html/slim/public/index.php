c00c91eb4466546fb9f221bb12a227e0830bed56
phacman <phacman@yandex.ru>
2024-07-28 05:32:18 +0300

    catch build

--- html/slim/public/index.php
+++ html/slim/public/index.php
@@ -9,6 +9,8 @@ use App\Application\Settings\SettingsInterface;
 use DI\ContainerBuilder;
 use Slim\Factory\AppFactory;
 use Slim\Factory\ServerRequestCreatorFactory;
+use Slim\Psr7\Headers;
+use Slim\Psr7\Response as SlimResponse;
 
 $dirname = dirname(__DIR__);
 require $dirname.'/vendor/autoload.php';
@@ -33,52 +35,59 @@ $dependencies($containerBuilder);
 $repositories = require $dirname.'/app/repositories.php';
 $repositories($containerBuilder);
 
-// Build PHP-DI Container instance
-$container = $containerBuilder->build();
+try {
+    // Build PHP-DI Container instance
+    $container = $containerBuilder->build();
+    // Instantiate the app
+    AppFactory::setContainer($container);
+    $app = AppFactory::create();
+    $callableResolver = $app->getCallableResolver();
 
-// Instantiate the app
-AppFactory::setContainer($container);
-$app = AppFactory::create();
-$callableResolver = $app->getCallableResolver();
+    // Register middleware
+    $middleware = require $dirname.'/app/middleware.php';
+    $middleware($app);
 
-// Register middleware
-$middleware = require $dirname.'/app/middleware.php';
-$middleware($app);
+    // Register routes
+    $routes = require $dirname.'/app/routes.php';
+    $routes($app);
 
-// Register routes
-$routes = require $dirname.'/app/routes.php';
-$routes($app);
+    /** @var SettingsInterface $settings */
+    $settings = $container->get(SettingsInterface::class);
 
-/** @var SettingsInterface $settings */
-$settings = $container->get(SettingsInterface::class);
+    $displayErrorDetails = $settings->get('displayErrorDetails');
+    $logError = $settings->get('logError');
+    $logErrorDetails = $settings->get('logErrorDetails');
 
-$displayErrorDetails = $settings->get('displayErrorDetails');
-$logError = $settings->get('logError');
-$logErrorDetails = $settings->get('logErrorDetails');
+    // Create Request object from globals
+    $serverRequestCreator = ServerRequestCreatorFactory::create();
+    $request = $serverRequestCreator->createServerRequestFromGlobals();
 
-// Create Request object from globals
-$serverRequestCreator = ServerRequestCreatorFactory::create();
-$request = $serverRequestCreator->createServerRequestFromGlobals();
+    // Create Error Handler
+    $responseFactory = $app->getResponseFactory();
+    $errorHandler = new HttpErrorHandler($callableResolver, $responseFactory);
 
-// Create Error Handler
-$responseFactory = $app->getResponseFactory();
-$errorHandler = new HttpErrorHandler($callableResolver, $responseFactory);
+    // Create Shutdown Handler
+    $shutdownHandler = new ShutdownHandler($request, $errorHandler, $displayErrorDetails);
+    register_shutdown_function($shutdownHandler);
 
-// Create Shutdown Handler
-$shutdownHandler = new ShutdownHandler($request, $errorHandler, $displayErrorDetails);
-register_shutdown_function($shutdownHandler);
+    // Add Routing Middleware
+    $app->addRoutingMiddleware();
 
-// Add Routing Middleware
-$app->addRoutingMiddleware();
+    // Add Body Parsing Middleware
+    $app->addBodyParsingMiddleware();
 
-// Add Body Parsing Middleware
-$app->addBodyParsingMiddleware();
+    // Add Error Middleware
+    $errorMiddleware = $app->addErrorMiddleware($displayErrorDetails, $logError, $logErrorDetails);
+    $errorMiddleware->setDefaultErrorHandler($errorHandler);
 
-// Add Error Middleware
-$errorMiddleware = $app->addErrorMiddleware($displayErrorDetails, $logError, $logErrorDetails);
-$errorMiddleware->setDefaultErrorHandler($errorHandler);
+    // Run App & Emit Response
+    $response = $app->handle($request);
+} catch (Exception $e) {
+    $response = new SlimResponse(
+        status: $e->getCode(),
+        headers: new Headers(['X-Exception' => $e->getMessage()]),
+    );
+}
 
-// Run App & Emit Response
-$response = $app->handle($request);
 $responseEmitter = new ResponseEmitter();
 $responseEmitter->emit($response);
